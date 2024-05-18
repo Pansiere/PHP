3a79bdc0c2e922414fcc524131e88b1e5ec7c970
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-05-18 13:54:10 +0000

    atualizar

--- php-primeiros-passos/7 - decisoes.php
+++ php-primeiros-passos/7 - decisoes.php
@@ -1,10 +1,17 @@
 <?php 
 
-$idade = 21;
+$idade = 16;
+$numeroDePessoas = 21;
 
-echo "Você só pode entrar se tiver mais do que 18 anos" . PHP_EOL;
+echo "Você só pode entrar se tiver mais do que 18 anos ou a partir de 16 anos acompanhado." . PHP_EOL;
 
-if ($idade > 18) {
-    echo "Você tem $idade anos." . PHP_EOL;
-    echo 'Pode entrar';
+if ($idade >= 18)
+    echo "Você tem $idade anos. Pode entrar sozinho";
+elseif ($idade >= 16 && $numeroDePessoas > 1)
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
+else{
+    echo "Você só tem $idade anos. Você não pode entrar";
+    echo PHP_EOL . "É uma pena.";
 }
+echo PHP_EOL;
+echo "Adeus!";
