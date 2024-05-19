6b576609761c1ed3b5cf949b8212e6e923fd8e79
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-05-19 00:00:41 +0000

    nada a declarar

--- php-primeiros-passos/Desafios/1 - numeros-impares.php
+++ php-primeiros-passos/Desafios/1 - numeros-impares.php
@@ -1,6 +1,6 @@
 <?php
-    for ($contador = 1; $contador < 100; $contador++) {
-        if ($contador % 2 != 0)
-            echo "$contador\n";
+    for ($i = 1; $i < 100; $i++) {
+        if ($i % 2 != 0)
+            echo "$i\n";
     }
 ?>
---
+++ php-primeiros-passos/Desafios/3 - IMC.php
@@ -0,0 +1,6 @@
+<?php
+    $peso = 70;
+    $altura = 1.7;
+    $IMC = $peso / $altura ** 2; 
+    echo $IMC
+?>
