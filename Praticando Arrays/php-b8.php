84c64f24423e8bde97bee9dfee85bd3583520514
Joao Pedro <ojp027@gmail.com>
2024-06-01 08:30:46 +0000

    update

--- Praticando Arrays/php-b8.php
+++ Praticando Arrays/php-b8.php
@@ -6,23 +6,31 @@
 4. Após calcular o valor que cada pessoa deve pagar, imprima o resultado, indicando quanto cada uma deve contribuir para o churrasco.
 5. Imprima qual foi o item mais caro do churrasco.
 6. Se o número de participantes for igual ou menor que um, imprima uma mensagem. “O churrasco foi cancelado, todo mundo furou!”*/
-    function calcularMedia(array $notas) : float {
-        $somas = 0;
-        for($i = 0; $i <= 4; $i++){
-            $somas = $somas + $notas[$i];
-        }
-        $mediasDasNotas = $somas / 5;
-        return $mediasDasNotas;
+function valorPorPessoa($valorTodosItens, $numeroTotalParticipantes): float
+{
+    $valorCadaPessoaDevePagar = $valorTodosItens / $numeroTotalParticipantes;
+    return $valorCadaPessoaDevePagar;
+}
+
+$itensComprados = ['carvao', 'carne', 'calabresa', 'refrigerantes', 'temperos'];
+$precoItens = [30, 60, 35, 30, 15];
+$totalPrecoItens = 0;
+$itemMaisCaroDois = 0;
+
+for ($i = 0; $i < count($precoItens); $i++) {
+    $totalPrecoItens = $precoItens[$i] + $totalPrecoItens;
+    $itemMaisCaro = $precoItens[$i];
+    if($precoItens[$i] > $itemMaisCaro){
+        $itemMaisCaroDois = $i;
+        return $itemMaisCaroDois;
     }
-    $notasAlunos = [
-        [8.5, 6.0, 7.8, 9.2, 5.5], // Aluno 1
-        [7.0, 8.0, 6.5, 7.5, 8.5], // Aluno 2
-        [6.5, 7.5, 4.5, 5.5, 7.0], // Aluno 3
-        [5.0, 4.6, 7.8, 9.0, 6.0]  // Aluno 4
-    ];
-    foreach ($notasAlunos as $indiceAluno => $notas) {
-        $media = calcularMedia($notas);
-        echo "A média do Aluno " . ($indiceAluno + 1) . " é de: " . $media . PHP_EOL;
-    }
-//passei umas 3hrs nesse código. Acho q agr foi :')
-?>
+}
+echo "Qual o numero total de participantes? ";
+$numeroParticipantes = trim(fgets(STDIN));
+$resultado = valorPorPessoa($totalPrecoItens, $numeroParticipantes);
+
+if ($numeroParticipantes < 2) {
+    echo "O churrasco foi cancelado, todo mundo furou!";
+}else {
+    echo "Cada pessoa deve contribuir para o churrasco com R$" . $resultado . ",00.\nO item mais caro do churrasco foi o $itemMaisCaro";
+}
