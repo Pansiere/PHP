d1b34d2f44bd53fc3d9ee9e62f94946bee55ef8d
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-04 23:12:59 +0000

    Update

--- Praticando Arrays/php-b1/php-b1-15.php
+++ Praticando Arrays/php-b1/php-b1-15.php
@@ -3,6 +3,7 @@
     Descrição: Escreva um programa que leia um array de 7 elementos e um número. O programa deve imprimir se o número está presente no array e em que posição(ões).
 */
     echo "Programa que lê um array de 7 elementos e um número. E imprimi se o número está presente no array e em que posição(ões).\n";
+    $encontrado = false;
 
     for ($i = 0; $i <= 6; $i++){
         echo $i == 0 ? "Digite o 1° elemento ou um número: " : "Digite o " . ($i + 1) . "° elemento ou um número: ";
@@ -13,7 +14,11 @@
 
     for ($i = 0; $i < count($lista); $i++){
         if($lista[$i] == $numero){
-            echo "No índice  existe um número, que é numerosPresentes" . PHP_EOL;
+            echo "O número $numero está presente na lista, e está no índice $i." . PHP_EOL;
+            $encontrado = true;
         }
+    }
+    if ($encontrado != true){
+        echo "O número $numero não existe na lista.";
     }
 ?>
