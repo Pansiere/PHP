34647c3807b46d857602480d01bfa22eee1c9e58
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-15 16:17:50 +0000

    arrays.php

--- arrays.php
+++ arrays.php
@@ -1,10 +1,11 @@
 <?php
 $array = [];
 $array[] = ['Valor' => 5];
-$greeting = "Hello\n";
 
+$greeting = "Hello\n";
 echo $greeting;
 
 foreach ($array as $key => $numeros) {
+
     echo 'O índice ' . $key . ' tem o valor de: ' . $numeros['Valor'] . PHP_EOL;
 }
