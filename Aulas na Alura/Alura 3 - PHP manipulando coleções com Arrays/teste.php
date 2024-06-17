ad8624f3b9c56f30302cc6f149cd8555a443cf1f
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-17 22:43:24 +0000

    update

---
+++ Aulas na Alura/Alura 3 - PHP manipulando coleções com Arrays/teste.php
@@ -0,0 +1,16 @@
+<?php
+$array = [
+    1 => 'um'
+];
+
+for ($i = 1; $i <= 6; $i++) {
+    echo $i . 'º' . ' olá.' . PHP_EOL;
+}
+
+echo PHP_EOL;
+function teste()
+{
+    echo 'Teste';
+}
+
+teste();
