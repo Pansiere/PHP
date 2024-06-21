2f887779e44aee034f5ab337c94ab04a0643f875
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-06-21 20:18:40 -0300

    update

--- Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/21 - strtr.php
+++ Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/21 - strtr.php
@@ -1,20 +1,23 @@
 <?php
 
-//substituindo uma palavra e removendo o resto:
 $texto = 'Texto com qualquer coisa poxa e caramba';
+
+//substituindo uma palavra e removendo o resto
 echo str_replace(
     ['poxa', 'caramba'],
     ['****'],
     $texto
 ) . PHP_EOL;;
 
-//translating - trabalha com caractéres, e não com strings
+//strtr = string translate
+//trabalha com caractéres, e não com strings
 echo strtr(
     $texto,
     'poxa',
     '***@'
 ) . PHP_EOL;
 
+//mudando palavras encontradas no $texto para outras novas
 echo strtr(
     $texto,
     [
--- Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/24 - .php
+++ Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/24 - .php
@@ -17,7 +17,3 @@ echo "strtr:\n" . strtr($massege, $trans) . PHP_EOL;
 
 //o 'str_replace' não respeita as alterações já feitas
 echo "\nstr_replace:\n" . str_replace(['hello', 'hi'], ['hi', 'hello'], $massege);
-
-//exemplo strtr com meu primeiro nome
-$primeiroNome = 'Joao';
-echo PHP_EOL . strtr($primeiroNome, 'a', 'ã') . PHP_EOL;
