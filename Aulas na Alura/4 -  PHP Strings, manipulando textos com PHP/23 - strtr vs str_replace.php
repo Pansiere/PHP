45179908849e6ba7975d4af3366f82c36cf18b7d
Joao Pedro V. Pansiere <pansiere2001@gmail.com>
2024-06-22 11:07:46 -0300

    Alura 4

--- Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/23 - strtr vs str_replace.php
+++ Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/23 - strtr vs str_replace.php
@@ -6,6 +6,7 @@ strtr(string $string, array $replace_pairs): string
 */
 
 $massege = 'hi all, I said hello';
+$primeiroNome = 'Joao';
 
 $trans = [
     "hello" => "hi",
@@ -13,12 +14,16 @@ $trans = [
 ];
 
 //o 'strtr' respeita as alterações já feitas
-echo "strtr:\n" . strtr($massege, $trans) . PHP_EOL;
+echo "strtr:\n" .
+    strtr($massege, $trans) .
+    PHP_EOL;
 
 //o 'str_replace' não respeita as alterações já feitas
-echo "\nstr_replace:\n" . str_replace(['hello', 'hi'], ['hi', 'hello'], $massege) . PHP_EOL;
+echo "\nstr_replace:\n" .
+    str_replace(['hello', 'hi'], ['hi', 'hello'], $massege) .
+    PHP_EOL;
 
 //exemplo str_replace com meu primeiro nome
-$primeiroNome = 'Joao';
-echo "\nstr_replace: adicioando '~' no 'a' em $primeiroNome\n"
-    . str_replace('a', 'ã', 'Joao') . PHP_EOL;
+echo "\nstr_replace: adicionando '~' no 'a' em $primeiroNome\n" .
+    str_replace('a', 'ã', 'Joao') .
+    PHP_EOL;
