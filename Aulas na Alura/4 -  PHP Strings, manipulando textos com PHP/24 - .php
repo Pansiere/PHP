a93ba040e583fa042f7a116ac151b7f5aaec68ed
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-06-21 21:22:20 -0300

    update

--- Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/24 - .php
+++ Aulas na Alura/4 -  PHP Strings, manipulando textos com PHP/24 - .php
@@ -1,6 +1,17 @@
 <?php
 
-$primeiroNome = 'João';
+$url = 'https://pansiere.com.br/';
 
-//exemplo strtr com meu primeiro nome
-echo PHP_EOL . strtr($primeiroNome, 'a', 'ã') . PHP_EOL;
+if (str_starts_with($url, 'https')) {
+    echo "$url é uma url segua.\n";
+} else {
+    echo "\n$url não uma url segura.";
+}
+
+echo PHP_EOL;
+
+if (str_ends_with($url, '.br') || str_ends_with($url, '.br/')) {
+    echo "$url é um domínio do Brasil.\n";
+} else {
+    echo "$url não é um domínio do Brasil.";
+}
