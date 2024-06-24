f9b69673e3183d4d5dffd78fca95d2a1b5d265f2
João Pedro Vicente Pansiere <ojp027@gmail.com>
2024-06-24 20:54:49 -0300

    update

--- Projetos/PHP & MySQL/config.php
+++ Projetos/PHP & MySQL/config.php
@@ -1,32 +1,14 @@
 <?php
-// Incluindo arquivos de funções e classes
-require_once 'includes/functions.php';
-require_once 'includes/classes/User.php';
+$servername = getenv('DB_SERVER');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
 
-// Definindo constantes para conexão com banco de dados
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'meu_banco');
+// Criar a conexão
+$conn = new mysqli($servername, $username, $password, $dbname, null, $socket);
 
-// Iniciando a sessão
-session_start();
-
-// Configurações adicionais
-$siteName = 'Minha Página PHP';
-$adminEmail = 'admin@exemplo.com';
-
-$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-
-// session_start();
-// if (!isset($_SESSION['user_id'])) {
-//     header('Location: login.php');
-//     exit;
-// }
-
-try {
-    // código que pode lançar uma exceção
-} catch (Exception $e) {
-    error_log($e->getMessage());
-    echo 'Ocorreu um erro, por favor tente novamente mais tarde.';
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
 }
+echo "Conectado com sucesso!";
