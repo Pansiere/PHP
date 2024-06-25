e9d16d31389bc221c7c3773737074526ae989776
João Pedro V. Pansiere <ojp027@gmail.com>
2024-06-25 19:03:00 +0000

    update

---
+++ phpdocker(1)/phpdocker/public/includes/functions.php
@@ -0,0 +1,14 @@
+<?php
+function getClientes($conn)
+{
+    $sql = "SELECT CPF, NOME, ENDERECO1, ENDERECO2, BAIRRO, CIDADE, ESTADO, CEP, IDADE, SEXO, LIMITE_CREDITO, VOLUME_COMPRA, PRIMEIRA_COMPRA, DATA_NASCIMENTO FROM jpCliente";
+    $result = $conn->query($sql);
+
+    $clientes = [];
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $clientes[] = $row;
+        }
+    }
+    return $clientes;
+}
---
+++ phpdocker(1)/phpdocker/public/index.php
@@ -0,0 +1,7 @@
+<?php include 'includes/header.php'; ?>
+
+<div class="content">
+    <p>Este é o conteúdo principal da página.</p>
+</div>
+<?php $clissentes = getClientes($conn); ?>
+<?php include 'includes/footer.php'; ?>
