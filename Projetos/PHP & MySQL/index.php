4805bd16459422d625d4aa52a3ea376bd4e8bbea
João Pedro V. Pansiere <ojp027@gmail.com>
2024-06-25 20:08:18 +0000

    update

--- Projetos/PHP & MySQL/index.php
+++
@@ -1,7 +0,0 @@
-<?php include 'includes/header.php'; ?>
-
-<div class="content">
-    <p>Este é o conteúdo principal da página.</p>
-</div>
-<?php $clissentes = getClientes($conn); ?>
-<?php include 'includes/footer.php'; ?>
