d45ce21193cdbc96ef67218473ba14a3e776bb99
João Pedro V. Pansiere <ojp027@gmail.com>
2024-06-26 17:19:16 +0000

    update

--- php-development-environment/public/config.php
+++ php-development-environment/public/config.php
@@ -3,30 +3,17 @@
 require_once 'includes/functions.php';
 require_once 'includes/classes/User.php';
 
-// Definindo constantes para conexão com banco de dados
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'meu_banco');
+$hostname = "mysql";
+$username = "php";
+$password = "password";
+$database = "DB";
 
-// Iniciando a sessão
-session_start();
+// Criar a conexão com o MySQL usando MySQLi
+$conn = new mysqli($hostname, $username, $password, $database);
 
-// Configurações adicionais
-$siteName = 'Minha Página PHP';
-$adminEmail = 'admin@exemplo.com';
-
-$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
-
-// session_start();
-// if (!isset($_SESSION['user_id'])) {
-//     header('Location: login.php');
-//     exit;
-// }
-
-try {
-    // código que pode lançar uma exceção
-} catch (Exception $e) {
-    error_log($e->getMessage());
-    echo 'Ocorreu um erro, por favor tente novamente mais tarde.';
+// Verificar a conexão
+if ($conn->connect_error) {
+    die("Conexão falhou: " . $conn->connect_error);
 }
+
+echo "\nConectado ao banco de dados com sucesso!\n" . PHP_EOL;
