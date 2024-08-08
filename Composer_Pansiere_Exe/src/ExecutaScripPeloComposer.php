39bc440245efc6559468fe21f0181e720e6f6b46
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-08-08 10:04:24 -0300

    update

--- Composer_Pansiere_Exe/src/ExecutaScripPeloComposer.php
+++ Composer_Pansiere_Exe/src/ExecutaScripPeloComposer.php
@@ -2,13 +2,17 @@
 
 namespace Pansiere\Exe;
 
-#use PHPUnit\Framework\TestCase; //testing phpunit
+#use PHPUnit\Framework\TestCase; // for testing phpunit
 
-class ExecutaScripPeloComposer # extends TestCase //testing phpunit
+class ExecutaScripPeloComposer # extends TestCase // for testing phpunit
 {
     public static function mensagem()
     {
-        $mensagem = 'Testando';
-        echo $mensagem . PHP_EOL;
+        $mensagem = "\n";
+        $mensagem .= str_repeat("#", 47) . "\n";
+        $mensagem .= "# " . str_pad("Mensagem programada exibida com sucesso!!", 43, " ", STR_PAD_BOTH) . " #\n";
+        $mensagem .= str_repeat("#", 47) . "\n\n";
+
+        echo $mensagem;
     }
 }
