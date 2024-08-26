e5d257f467492af2f20b47a7d67d25926429aac3
João Pedro V. Pansiere <ojp027@gmail.com>
2024-08-26 20:24:27 -0300

    update

--- Projetos/MarFit/14 - PHP e MySQL criando sua primeira aplicação web/10 - biblioteca DomPdf/public/excluir-produto.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'src/code.php';
-
-$produtoRepositorio->deletar($_POST['id']);
-
-header("Location: admin.php");
--- Projetos/MarFit/GG/14 - PHP e MySQL criando sua primeira aplicação web/11 - Projeto final do curso/src/conexao-bd.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$pdo = new PDO('mysql:host=localhost;dbname=serenatto', 'root', '1234');
